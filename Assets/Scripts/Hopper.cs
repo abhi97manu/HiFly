ccc07542904680bc7810d12c999a3fb65b628a79
Abhishek Kumar <abhi3144.manu@gmail.com>
2024-05-06 08:29:46 +0100

    Initial Commit

--- Assets/Scripts/Hopper.cs
+++ Assets/Scripts/Hopper.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 using static UnityEngine.RuleTile.TilingRuleOutput;
 
 public class Hopper : MonoBehaviour
 {
+    FlyMovement fly;
+
     float velocity = 2.0f;
     float gravityAcc = 0.5f;
+    float timer = 0;
     Vector3 InitPos;
     Vector3 currentPos;
     Vector3 hop;
@@ -14,29 +18,44 @@ public class Hopper : MonoBehaviour
     {
         InitPos = transform.position;
         currentPos = Vector3.zero;
+        fly = FindAnyObjectByType<FlyMovement>();
        
     }
 
     // Update is called once per frame
     void Update()
-    {
-        HoppingFunc();
-        if ((transform.position.y <= -3.5f))
+    { 
+        float dist = this.transform.position.x - fly.transform.position.x;
+
+      if(dist <5.0f ) {
+            HoppingFunc();
+
+        }
+           
+          
+        
+        if ((transform.position.y <= -5.5f))
         {
+
+            Destroy(this.gameObject);
+        }
             //currentPos = transform.position;
 
-            InitPos = new Vector3(transform.position.x, -3.4f, 0); ;
-            HoppingFunc();
-        }
+        //    InitPos = new Vector3(transform.position.x, -3.4f, 0); ;
+        //    HoppingFunc();
+        //}
       
 
     }
 
     void HoppingFunc()
     {
-        Debug.Log(InitPos);
-         hop = new Vector3(-velocity * Mathf.Cos(45f * Mathf.Deg2Rad) * Time.time, (velocity * Mathf.Sin(45f * Mathf.Deg2Rad) * Time.time) - (0.5f * gravityAcc * Time.time * Time.time), 0);
+
         
-        transform.position = hop + InitPos ;
+        timer += 1f * Time.deltaTime;
+            hop = new Vector3(-velocity * Mathf.Cos(45f * Mathf.Deg2Rad) * timer, (velocity * Mathf.Sin(45f * Mathf.Deg2Rad) * timer) - (0.5f * gravityAcc * timer * timer), 0);
+        Debug.Log(timer);
+        this.transform.position = InitPos + hop;
+        
     }
 }
